c46ae39ecce47bbcc75a7f078d98bbbfcb1f23b7
Vladi Angelov <vladislav_angelov@yahoo.com>
2021-07-16 11:51:08 +0100

    feat: Add cypress for end-to-end testing

--- package-scripts.js
+++ package-scripts.js
@@ -1,8 +1,9 @@
 module.exports = {
   scripts: {
-    default: 'react-scripts start',
-    build: 'react-scripts build',
-    test: 'react-scripts test',
-    eject: 'react-scripts eject'
-  }
-};
+    default: "react-scripts start",
+    build: "react-scripts build",
+    test: "react-scripts test",
+    eject: "react-scripts eject",
+    cypress: "yarn run cypress open",
+  },
+}
